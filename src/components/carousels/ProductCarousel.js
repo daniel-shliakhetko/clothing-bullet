cbd7332c2155fb1f0172bb060acb01ec66f457af
Daniel-Shliakhetko <danielshliakhetko@gmail.com>
2023-06-27 13:29:38 +0300

    Add ProductCarousel

---
+++ src/components/carousels/ProductCarousel.js
@@ -0,0 +1,69 @@
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import { Navigation } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import ProductCard from "../cards/ProductCard";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const ProductCarousel = (props) => {
+  const sliderRef = useRef();
+
+  return (
+    <div className="flex items-center">
+      <button onClick={() => sliderRef.current?.slidePrev()}>
+        <FontAwesomeIcon
+          className="h-8 w-8 text-indigo-500 pl-2"
+          icon={faArrowLeft}
+        />
+      </button>
+      <Swiper
+        onSwiper={(it) => (sliderRef.current = it)}
+        modules={[Navigation]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation={{
+          prevEl: "",
+          nextEl: "",
+        }}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+          },
+          1100: {
+            slidesPerView: 3,
+          },
+          1440: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {props.products.map((product) => (
+          <SwiperSlide>
+            <div className="my-6 flex justify-center">
+              <ProductCard product={product} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <button onClick={() => sliderRef.current?.slideNext()}>
+        <FontAwesomeIcon
+          className="h-8 w-8 text-indigo-500 pr-2"
+          icon={faArrowRight}
+        />
+      </button>
+    </div>
+  );
+};
+
+ProductCarousel.propTypes = {
+  products: PropTypes.object,
+};
+
+export default ProductCarousel;
--- src/pages/FrontPage/FrontPage.js
+++ src/pages/FrontPage/FrontPage.js
@@ -3,6 +3,7 @@ import { Introduction } from "./sections/Introduction";
 import ProductList from "../../components/lists/ProductList";
 import { useProducts } from "../../contexts/ProductsContext";
 import HoverableHeading from "../../components/headings/HoverableHeading";
+import ProductCarousel from "../../components/carousels/ProductCarousel";
 
 export const FrontPage = () => {
   const products = useProducts();
@@ -15,6 +16,10 @@ export const FrontPage = () => {
       <section className="w-full bg-indigo-400">
         <HoverableHeading title="Products" fontSize="1.5em" colorClassName="text-indigo-100 text-center w-full" className="hover:text-indigo-200 pb-8"/>
       <ProductList products={products} dark={true}/>
+      </section>
+      <section className="w-full py-8">
+        <HoverableHeading title="On Sale" fontSize="1.5em" colorClassName="text-indigo-500 text-center w-full" className="hover:text-indigo-500 pb-8"/>
+        <ProductCarousel products={products.filter((product)=>product.discount!==0)}/>
       </section>
     </>
   );
